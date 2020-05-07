ee95836aa599b38f9fe91bc7290671457e7ea9bd
Jarvis <yashavant7@gmail.com>
2020-05-06 22:06:39 -0700

    search completed

--- car-rental-api/src/main/java/com/wip/carrental/controller/VehicleController.java
+++ car-rental-api/src/main/java/com/wip/carrental/controller/VehicleController.java
@@ -8,6 +8,7 @@ import java.util.stream.Collectors;
 
 import com.wip.carrental.controller.exceptions.ResourceNotFoundException;
 import com.wip.carrental.model.Reservation;
+import com.wip.carrental.model.SearchRequestBody;
 import com.wip.carrental.repository.ReservationRepository;
 import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,79 +28,103 @@ import com.wip.carrental.repository.VehicleRepository;
 @CrossOrigin(origins = "http://localhost:3000")
 public class VehicleController {
 
-	@Autowired
-	private VehicleRepository vehicleRepository;
-	
-	@Autowired
-	private ParkingLocationRepository parkingLocationRepository;
+    @Autowired
+    private VehicleRepository vehicleRepository;
 
-	@Autowired
-	private ReservationRepository reservationRepository;
+    @Autowired
+    private ParkingLocationRepository parkingLocationRepository;
 
-	@GetMapping("/vehicles")
-	public List<Vehicle> getAllVehicles() {
-		return (List<Vehicle>) vehicleRepository.findAll();
-	}
+    @Autowired
+    private ReservationRepository reservationRepository;
 
-	@GetMapping("/vehicles/{vehicleId}")
-	public Optional<Vehicle> getAllVehicles(@PathVariable Long vehicleId) {
-		return vehicleRepository.findById(vehicleId);
-	}
+    @GetMapping("/vehicles")
+    public List<Vehicle> getAllVehicles() {
+        return (List<Vehicle>) vehicleRepository.findAll();
+    }
 
-
-	//Get average reviews for vehicle
-    @GetMapping("/vehicles/{vehicleId}/reviews")
-    public float getAverageRatingForVehicle(@PathVariable Long vehicleId) {
-    	Iterable<Reservation> reservation = reservationRepository.findAll();
-    	float totalRating = 0;
-    	int countRating = 0;
-    	while(reservation.iterator().hasNext()) {
-    		Reservation r = reservation.iterator().next();
-    		if(r.getVehicle().getVehicleId() == vehicleId) {
-    			totalRating += r.getReview().getRating();
-    			countRating++;
-    		}
-    	}
-
-    	return totalRating/countRating;
+    @GetMapping("/vehicles/{vehicleId}")
+    public Optional<Vehicle> getAllVehicles(@PathVariable Long vehicleId) {
+        return vehicleRepository.findById(vehicleId);
     }
 
 
+    //Get average reviews for vehicle
+    @GetMapping("/vehicles/{vehicleId}/reviews")
+    public float getAverageRatingForVehicle(@PathVariable Long vehicleId) {
+        Iterable<Reservation> reservation = reservationRepository.findAll();
+        float totalRating = 0;
+        int countRating = 0;
+        while (reservation.iterator().hasNext()) {
+            Reservation r = reservation.iterator().next();
+            if (r.getVehicle().getVehicleId() == vehicleId) {
+                totalRating += r.getReview().getRating();
+                countRating++;
+            }
+        }
 
-	//Need to track parking location capacity here when adding a vehicle
-	@PostMapping("/vehicles/{locationid}")
-	public ResponseEntity<?> postVehicle(@RequestBody Vehicle vehicle, @PathVariable Long locationid) {
-		
-		ParkingLocation parkingLocation = parkingLocationRepository.findById(locationid).orElse(null);
-		
-		if(parkingLocation != null ) {
-			if(parkingLocation.getCapacity() > parkingLocation.getFilledSpots()) {
-				vehicle.setParkingLocation(parkingLocation);
-				parkingLocation.setFilledSpots(parkingLocation.getFilledSpots() + 1);
-				return ResponseEntity.ok(vehicleRepository.save(vehicle)); 
-			} else {
-				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Parking location is full");
-			}
-			
-		} else {
-			throw new ResourceNotFoundException("Parking Location with ID " + locationid + " not found");
-		}
-		
-	}
+        return totalRating / countRating;
+    }
+
+
+    //Need to track parking location capacity here when adding a vehicle
+    @PostMapping("/vehicles/{locationid}")
+    public ResponseEntity<?> postVehicle(@RequestBody Vehicle vehicle, @PathVariable Long locationid) {
+
+        ParkingLocation parkingLocation = parkingLocationRepository.findById(locationid).orElse(null);
+
+        if (parkingLocation != null) {
+            if (parkingLocation.getCapacity() > parkingLocation.getFilledSpots()) {
+                vehicle.setParkingLocation(parkingLocation);
+                parkingLocation.setFilledSpots(parkingLocation.getFilledSpots() + 1);
+                return ResponseEntity.ok(vehicleRepository.save(vehicle));
+            } else {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Parking location is full");
+            }
+
+        } else {
+            throw new ResourceNotFoundException("Parking Location with ID " + locationid + " not found");
+        }
+
+    }
 
     @PostMapping("/vehicles/search")
-    public ResponseEntity<?> SearchVehiclesByLocation(@RequestBody ParkingLocation location) {
+    public ResponseEntity<?> SearchVehicles(@RequestBody SearchRequestBody location) {
 
         try {
             ArrayList<ParkingLocation> parkingLocations = (ArrayList<ParkingLocation>) parkingLocationRepository.findAll();
 
-            List<ParkingLocation> cityLocations = parkingLocations.stream().filter(s -> s.getCity().equalsIgnoreCase(location.getCity())).collect(Collectors.toList());
+            String requestCity = location.getCity()== null ? "" : location.getCity();
+            String type = location.getType() == null ? "" : location.getType();
+            List<ParkingLocation> cityLocations = parkingLocations.stream().filter(s -> s.getCity().toLowerCase().contains(requestCity.toLowerCase())).collect(Collectors.toList());
 
             ArrayList<Vehicle> result = new ArrayList<>();
 
-            for (ParkingLocation city : cityLocations) {
-                result.addAll(Objects.requireNonNull(vehicleRepository.findAllByParkingLocation(city).orElse(null)));
+            if (type.equalsIgnoreCase("sedan") || type.equalsIgnoreCase("truck") || type.equalsIgnoreCase("suv") || type.equalsIgnoreCase("small")) {
+                for (ParkingLocation city : cityLocations) {
+                    ArrayList<Vehicle> vehicles = (ArrayList<Vehicle>) vehicleRepository.findAllByParkingLocation(city).orElse(null);
 
+                    if (vehicles != null) {
+                        result.addAll(Objects.requireNonNull(vehicles.stream().filter(s -> s.getVehicleType().toString().equalsIgnoreCase(type)).collect(Collectors.toList())));
+                    }
+                }
+
+                if (result.size() == 0) {
+                    for (ParkingLocation city : parkingLocations) {
+                        ArrayList<Vehicle> vehicles = (ArrayList<Vehicle>) vehicleRepository.findAllByParkingLocation(city).orElse(null);
+
+                        if (vehicles != null) {
+                            result.addAll(Objects.requireNonNull(vehicles.stream().filter(s -> s.getVehicleType().toString().equalsIgnoreCase(type)).collect(Collectors.toList())));
+                        }
+                    }
+                }
+            } else {
+                for (ParkingLocation city : cityLocations) {
+                    ArrayList<Vehicle> vehicles = (ArrayList<Vehicle>) vehicleRepository.findAllByParkingLocation(city).orElse(null);
+
+                    if (vehicles != null) {
+                        result.addAll(Objects.requireNonNull(vehicles));
+                    }
+                }
             }
 
             return ResponseEntity.ok(result);
@@ -107,36 +132,33 @@ public class VehicleController {
             throw new ResourceNotFoundException(e.getMessage());
 
         }
+    }
 
-
+    @PutMapping("/vehicles/{vehicleId}")
+    public ResponseEntity<?> updateVehicle(@PathVariable Long vehicleId, @RequestBody Vehicle vehicleRequestBody) {
+        return vehicleRepository.findById(vehicleId).map(vehicle -> {
+            vehicle.setVehicleName(vehicleRequestBody.getVehicleName());
+            vehicle.setDescription(vehicleRequestBody.getDescription());
+            vehicle.setVehicleBasePrice(vehicleRequestBody.getVehicleBasePrice());
+            vehicle.setVehicleType(vehicleRequestBody.getVehicleType());
+            vehicle.setParkingLocation(vehicleRequestBody.getParkingLocation());
+            vehicle.setStatus(vehicleRequestBody.getStatus());
+            return ResponseEntity.ok(vehicleRepository.save(vehicle));
+        }).orElseThrow(() -> new ResourceNotFoundException("Vehicle ID " + vehicleId + " not found"));
     }
 
 
-	@PutMapping("/vehicles/{vehicleId}")
-	public ResponseEntity<?> updateVehicle(@PathVariable Long vehicleId, @RequestBody Vehicle vehicleRequestBody) {
-		return vehicleRepository.findById(vehicleId).map(vehicle -> {
-			vehicle.setVehicleName(vehicleRequestBody.getVehicleName());
-			vehicle.setDescription(vehicleRequestBody.getDescription());
-			vehicle.setVehicleBasePrice(vehicleRequestBody.getVehicleBasePrice());
-			vehicle.setVehicleType(vehicleRequestBody.getVehicleType());
-			vehicle.setParkingLocation(vehicleRequestBody.getParkingLocation());
-			vehicle.setStatus(vehicleRequestBody.getStatus());
-			return ResponseEntity.ok(vehicleRepository.save(vehicle));
-		}).orElseThrow(() -> new ResourceNotFoundException("Vehicle ID " + vehicleId + " not found"));
-	}
-	
-	
-	@DeleteMapping("vehicles/{vehicleId}")
-	public ResponseEntity<?> deleteVehicle(@PathVariable Long vehicleId) {
-		if (vehicleRepository.existsById(vehicleId)) {
-			Vehicle vehicle = vehicleRepository.findById(vehicleId).orElse(null);
-			ParkingLocation parkingLocation = vehicle.getParkingLocation();
-			parkingLocation.setFilledSpots(parkingLocation.getFilledSpots() - 1);
-			vehicleRepository.deleteById(vehicleId);
-			
-			return ResponseEntity.ok("Vehicle with " + vehicleId + " deleted");
-		}
-		throw new ResourceNotFoundException("Vehicle with " + vehicleId + " not found");
+    @DeleteMapping("vehicles/{vehicleId}")
+    public ResponseEntity<?> deleteVehicle(@PathVariable Long vehicleId) {
+        if (vehicleRepository.existsById(vehicleId)) {
+            Vehicle vehicle = vehicleRepository.findById(vehicleId).orElse(null);
+            ParkingLocation parkingLocation = vehicle.getParkingLocation();
+            parkingLocation.setFilledSpots(parkingLocation.getFilledSpots() - 1);
+            vehicleRepository.deleteById(vehicleId);
 
-	}
+            return ResponseEntity.ok("Vehicle with " + vehicleId + " deleted");
+        }
+        throw new ResourceNotFoundException("Vehicle with " + vehicleId + " not found");
+
+    }
 }
